f1760559ed1dd53cb829b63fd8f898bdb2d624cb
darrellsean <143312927+darrellsean@users.noreply.github.com>
2025-10-14 15:11:02 +0800

    changed some database properties

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="pantrypal login";
+$db="pantrypal";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
